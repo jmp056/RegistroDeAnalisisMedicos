a2e1b862b9544c4c63f2eefac8d5861694ced207
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2019-06-12 23:12:27 -0400

    Creando la BLL de analisis

---
+++ RegistroDeAnalisisMedicos/BLL/AnalisisBLL.cs
@@ -0,0 +1,120 @@
+﻿using RegistroDeAnalisisMedicos.DAL;
+using RegistroDeAnalisisMedicos.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace RegistroDeAnalisisMedicos.BLL
+{
+    public class AnalisisBLL
+    {
+        public static bool Guardar(Analisis analisis)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+            try
+            {
+                if (db.Analisis.Add(analisis) != null)
+                    paso = db.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return paso;
+        }
+
+        public static bool Modificar(Analisis analisis)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+            try
+            {
+                var Anterior = db.Analisis.Find(analisis.AnalisisId);
+                foreach (var item in Anterior.Detalle)
+                {
+                    if (!analisis.Detalle.Exists(d => d.DetalleId == item.DetalleId))
+                        db.Entry(item).State = EntityState.Deleted;
+                }
+                db.Entry(analisis).State = EntityState.Modified;
+                paso = (db.SaveChanges() > 0);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return paso;
+        }
+
+
+        public static bool Eliminar(int id)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+            try
+            {
+                var eliminar = db.Analisis.Find(id);
+                db.Entry(eliminar).State = EntityState.Deleted;
+                paso = db.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return paso;
+        }
+
+        public static Analisis Buscar(int id)
+        {
+            Contexto db = new Contexto();
+            Analisis analisis = new Analisis();
+            try
+            {
+                analisis = db.Analisis.Find(id);
+                analisis.Detalle.Count();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return analisis;
+        }
+
+        public static List<Analisis> GetList(Expression<Func<Analisis, bool>> analisis)
+        {
+            List<Analisis> Lista = new List<Analisis>();
+            Contexto db = new Contexto();
+            try
+            {
+                Lista = db.Analisis.Where(analisis).ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return Lista;
+        }
+    }
+}
