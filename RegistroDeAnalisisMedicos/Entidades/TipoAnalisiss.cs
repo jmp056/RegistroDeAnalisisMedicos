dd9da5abdc5ca725f4e371845685b3012cf7b145
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2019-06-12 22:44:51 -0400

    Creando la entidad TipoAnalisiss

---
+++ RegistroDeAnalisisMedicos/Entidades/TipoAnalisiss.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroDeAnalisisMedicos.Entidades
+{
+    public class TipoAnalisiss
+    {
+        [Key]
+        public int TipoAnalisisId { get; set; }
+        public string Descripcion { get; set; }
+        public TipoAnalisiss()
+        {
+            TipoAnalisisId = 0;
+            Descripcion = string.Empty;
+        }
+        public TipoAnalisiss(int tipoAnalisisId, string descripcion)
+        {
+            TipoAnalisisId = tipoAnalisisId;
+            Descripcion = descripcion;
+        }
+    }
+}
