fb5dd43fff023e31789a1117de9e1d222ce20042
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2019-06-12 22:51:16 -0400

    Creando la entidad analisis detalle

---
+++ RegistroDeAnalisisMedicos/Entidades/AnalisisDetalle.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroDeAnalisisMedicos.Entidades
+{
+    public class AnalisisDetalle
+    {
+        [Key]
+        public int AnalisisId { get; set; }
+        public int TipoAnalisisId { get; set; }
+        public string Detalle { get; set; }
+        public decimal Resultado { get; set; }
+
+        public AnalisisDetalle()
+        {
+            AnalisisId = 0;
+            TipoAnalisisId = 0;
+            Detalle = string.Empty;
+            Resultado = 0;
+        }
+        public AnalisisDetalle(int analisisId, int tipoAnalisisId, string detalle, decimal resultado)
+        {
+            AnalisisId = analisisId;
+            TipoAnalisisId = tipoAnalisisId;
+            Detalle = detalle;
+            Resultado = resultado;
+        }
+    }
+}
